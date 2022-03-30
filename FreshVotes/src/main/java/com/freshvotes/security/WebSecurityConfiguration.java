e1199b9b7f0f726d5643db556d807ae8dcb78388
Priyakeshavan <priyakeshavamurthy11@gmail.com>
2022-03-30 10:48:56 -0500

    getting stared with spring security

---
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.freshvotes.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
+{
+  @Override
+protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	auth.inMemoryAuthentication()
+        .withUser("priyakeshavamurthy11@gmail.com")
+        .password("Andal@2811")
+        .roles("USER");
+	
+}  
+  @Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/").permitAll()
+		.anyRequest().hasRole("USER").and()
+		.formLogin()
+		.loginPage("/login")
+		.permitAll()
+		.and()
+		.logout()
+		.logoutUrl("/logout")
+		.permitAll();
+		
+	}
+	}
+
