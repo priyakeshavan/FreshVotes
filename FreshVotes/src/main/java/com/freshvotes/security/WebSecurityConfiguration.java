1ddeedab202f207acd124d837fab3d3d1f7c465f
Priyakeshavan <priyakeshavamurthy11@gmail.com>
2022-04-02 06:50:14 -0500

    Leveraging a database for users

--- FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -1,28 +1,37 @@
 package com.freshvotes.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
   
-  @Bean
+	@Autowired
+  private UserDetailsService userDetailsService;
+
+@Bean
   public PasswordEncoder getPasswordEncoder() {
     return new BCryptPasswordEncoder();
   }
   
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-        .passwordEncoder(getPasswordEncoder())
-        .withUser("priyakeshavamurthy11@gmail.com")
-        .password(getPasswordEncoder().encode("andal"))
-        .roles("USER");
+	  auth
+	  .userDetailsService(userDetailsService)
+	  .passwordEncoder(getPasswordEncoder());
+	  
+//    auth.inMemoryAuthentication()
+//        .passwordEncoder(getPasswordEncoder())
+//        .withUser("priyakeshavamurthy11@gmail.com")
+//        .password(getPasswordEncoder().encode("andal"))
+//        .roles("USER");
   }
   
   @Override
