d02a77637ba29ba8016d16e9e7044d2968f00718
Priyakeshavan <priyakeshavamurthy11@gmail.com>
2022-04-04 13:06:21 -0500

    Creating a registration page (Part II)

--- FreshVotes/src/main/java/com/freshvotes/web/LoginController.java
+++ FreshVotes/src/main/java/com/freshvotes/web/LoginController.java
@@ -1,21 +1,29 @@
 package com.freshvotes.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.freshvotes.domain.User;
 
 @Controller
-public class LoginController
-{
-	//@RequestMapping(value="/login", method=RequestMethod.GET)
+public class LoginController {
+  
   @GetMapping("/login")
-  public String login() 
-  {
+  public String login() {
     return "login";
   }
+  
   @GetMapping("/register")
-	  public String register()
-	  {
-	  return "register";
+  public String register (ModelMap model) {
+    model.put("user", new User());
+    return "register";
+  }
+  
+  @PostMapping("/register")
+  public String registerPost (User user) {
+    System.out.println(user);
+    return "redirect:/register";
   }
 }
-
