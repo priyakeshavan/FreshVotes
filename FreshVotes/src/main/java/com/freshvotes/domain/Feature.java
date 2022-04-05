a1786d0747eb67b8658f1cddab058ce8fd1fe7d5
Priyakeshavan <priyakeshavamurthy11@gmail.com>
2022-04-04 21:38:11 -0500

    Adding a Product entity

--- FreshVotes/src/main/java/com/freshvotes/domain/Feature.java
+++ FreshVotes/src/main/java/com/freshvotes/domain/Feature.java
@@ -4,41 +4,56 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Feature
 {
-private Long id;
-private String title;
-private String description;
-private String status;
-
-@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-public Long getId() 
-{
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public String getTitle() {
-	return title;
-}
-public void setTitle(String title) {
-	this.title = title;
-}
-public String getDescription() {
-	return description;
-}
-public void setDescription(String description) {
-	this.description = description;
-}
-public String getStatus() {
-	return status;
-}
-public void setStatus(String status) {
-	this.status = status;
-}
-
+  private Long id;
+  private String title;
+  private String description;
+  private String status;
+  private Product product;
+  
+  @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+  public Long getId()
+  {
+    return id;
+  }
+  public void setId(Long id)
+  {
+    this.id = id;
+  }
+  public String getTitle()
+  {
+    return title;
+  }
+  public void setTitle(String title)
+  {
+    this.title = title;
+  }
+  public String getDescription()
+  {
+    return description;
+  }
+  public void setDescription(String description)
+  {
+    this.description = description;
+  }
+  public String getStatus()
+  {
+    return status;
+  }
+  public void setStatus(String status)
+  {
+    this.status = status;
+  }
+  
+  @ManyToOne
+  public Product getProduct() {
+    return product;
+  }
+  public void setProduct(Product product) {
+    this.product = product;
+  }
 }
